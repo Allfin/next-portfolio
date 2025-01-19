7f8382c125efe1ddb8197129d6a47faa9ec01d24
All Fine Maulinaro <allfinem10@gmail.com>
2025-01-19 11:37:10 +0700

    feat: add config

---
+++ config/index.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  title: "All Fine Maulianro — Frontend Developer",
+  author: "All Fine Maulianro",
+  description:
+    "Frontend Developer based in Indonesia. I specialize in building user-friendly web applications using Next.js, with expertise in HTML, CSS, and JavaScript.",
+  navLinks: [
+    { text: "Experience", href: "#experience" },
+    { text: "Projects", href: "#projects" },
+    { text: "About", href: "#about" },
+  ],
+};
