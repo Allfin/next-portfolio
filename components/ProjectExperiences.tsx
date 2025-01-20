ff67bad834b6e7e3514d9a3f0dd5744aba4e3368
All Fine Maulinaro <allfinem10@gmail.com>
2025-01-20 21:30:58 +0700

    style: edit card project

--- components/ProjectExperiences.tsx
+++ components/ProjectExperiences.tsx
@@ -11,34 +11,32 @@ const ProjectExperiences = () => {
 
       {siteContent.project.map((project, index) => (
         <div
-          className="mb-10 relative overflow-hidden w-full grid grid-rows-2 sm:grid-rows-1 sm:grid-cols-2"
+          className="mb-10 w-full grid grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 bg-gray-800 rounded-xl h-[580px] md:h-96 "
           key={index}
         >
-          <div className="flex-1 justify-end">
-            <h4 className="text-2xl font-serif font-semibold text-green-400 mb-1.5  ">
+          <div className="flex-1 px-6 pt-12">
+            <h4 className="text-3xl font-serif font-semibold text-green-400 mb-1.5  ">
               {project.nameProject}
             </h4>
-            <h3 className="text-1xl font-serif text-white font-medium mb-0.5">
+            <h3 className="text-2xl font-serif text-white font-medium mb-0.5">
               {project.summary}
             </h3>
-            <div className="flex">
-              <div className="flex mt-6">
-                <a className="flex text-1xl" href={project.source}>
-                  Source <ArrowUpRight />
-                </a>
-                <a className=" flex text-1xl" href={project.preview}>
-                  Preview <ArrowUpRight />
-                </a>
-              </div>
+            <div className="flex mt-6 pt-10">
+              <a className="flex text-1xl" href={project.source}>
+                Source <ArrowUpRight />
+              </a>
+              <a className=" flex text-1xl" href={project.preview}>
+                Preview <ArrowUpRight />
+              </a>
             </div>
           </div>
-          <div className="overflow-hidden">
+          <div className="flex items-end justify-end overflow-hidden">
             <Image
-              className="rounded-md"
+              className="w-[95%] sm:w-full h-full sm:h-[85%] object-none object-left-top rounded-md"
               alt="project"
               src={project.img}
-              width={736}
-              height={483}
+              width={500}
+              height={300}
             />
           </div>
         </div>
